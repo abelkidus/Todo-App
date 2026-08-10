b4c4174330b60cee5af403884876512b625d8afd
abelkidus <abelhadush18@gmail.com>
2026-08-10 21:28:27 +0300

    Implemented getting user input

---
+++ lib/pages/todo_page.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+
+class TodoPage extends StatefulWidget {
+  const TodoPage({super.key});
+
+  @override
+  State<TodoPage> createState() => _TodoPageState();
+}
+
+class _TodoPageState extends State<TodoPage> {
+
+  TextEditingController myController = TextEditingController();
+
+  void greetUser(){
+    print(myController.text);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Padding(
+          padding: const EdgeInsets.all(25.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              TextField(
+                controller: myController,
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(),
+                  hintText: "Type your name",
+                ),
+              ),
+              
+              ElevatedButton(onPressed: greetUser, child: Text("Tap")),
+          
+            ],
+          ),
+        ),),
+    );
+  }
+}
